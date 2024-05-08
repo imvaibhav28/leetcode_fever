c5275bcce1480cd6ec381de3d47f239d3b0b383a
Vaibhav Upadhyay <8084054+imvaibhav28@users.noreply.github.com>
2024-05-08 20:59:50 +0100

    Update 0197-rising-temperature.sql

--- 0197-rising-temperature/0197-rising-temperature.py
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -1,9 +1,20 @@
-import pandas as pd
+# import pandas as pd
 
 
-def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    weather.sort_values(by='recordDate', inplace=True)
-    return weather[
-        (weather.temperature.diff() > 0)
-      & (weather.recordDate.diff().dt.days == 1)
-    ][['id']]
+# def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+#     weather.sort_values(by='recordDate', inplace=True)
+#     return weather[
+#         (weather.temperature.diff() > 0)
+#       & (weather.recordDate.diff().dt.days == 1)
+#     ][['id']]
+
+
+# ORACLE:
+
+
+SELECT W.ID
+FROM Weather W
+JOIN Weather X
+ON W.recordDate = X.recordDate + INTERVAL '1' DAY
+WHERE W.temperature > X.temperature
+;
